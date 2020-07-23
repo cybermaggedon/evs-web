250556ea61cb4193a5e4b3bc8dee71182f1bfee9
Mark Adams <cybermaggedon@gmail.com>
2020-07-23 23:17:13 +0100

    Comments and minor refactor

--- src/app/threat.service.ts
+++ src/app/threat.service.ts
@@ -26,49 +26,48 @@ export class ThreatService {
 
     constructor(private tgSvc : ThreatGraphService) { }
 
+    groupThreats(g : Graph, id : string, limit : number) : Threats {
+
+	let dt = new Threats();  
+
+	let threats = new Map();
+
+	for (let elt of g.edges) {
+
+	    if (!threats.has(elt.group)) {
+		threats.set(elt.group, []);
+	    }
+
+	    let c = new Contact();
+
+	    if (elt.source == id)
+		c.id = elt.destination;
+	    else
+		c.id = elt.source;
+	    c.age = elt.earliest;
+
+	    threats.get(elt.group).push(c);
+
+	}
+
+	for (let kind of threats.keys()) {
+	    threats.get(kind).sort((a, b) => b.age - a.age);
+	    threats.set(kind, threats.get(kind).slice(0, limit));
+	}
+
+	dt.threats = threats;
+
+	return dt;
+
+    }
+
     // Async threat fetch.  Given a seed, find all threatgraph
     // relationships in a time period.
     getThreats(id : string, from : Date, to : Date, limit=25) :
     Observable<Threats> {
-
-	let obs = new Observable<Threats>();
-
-	const devicify = map((g : Graph) => {
-
-	    let dt = new Threats();
-
-	    let threats = new Map();
-
-	    for (let elt of g.edges) {
-
-		if (!threats.has(elt.group)) {
-		    threats.set(elt.group, []);
-		}
-
-		let c = new Contact();
-
-		if (elt.source == id)
-		    c.id = elt.destination;
-		else
-		    c.id = elt.source;
-		c.age = elt.earliest;
-
-		threats.get(elt.group).push(c);
-
-	    }
-
-	    for (let kind of threats.keys()) {
-		threats.get(kind).sort((a, b) => b.age - a.age);
-		threats.set(kind, threats.get(kind).slice(0, limit));
-	    }
-
-	    dt.threats = threats;
-
-	    return dt;
-
-	});
 	
-        return devicify(this.tgSvc.getThreats(id, from, to));
+        return this.tgSvc.getThreats(id, from, to).
+            pipe(map(g => this.groupThreats(g, id, limit)));
 
     }
     
--- src/app/window.service.ts
+++ src/app/window.service.ts
@@ -1,6 +1,12 @@
+
+// Window service, abstracts the window sliders, distributing slider updates.
+// This potentially permits multiple sliders, and multiple sources of updates
+
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 
+// A slider update emits a window.  The window contains the raw slider value
+// (hours duration) and an absolute date value.
 export class Window {
     constructor(value : number, earliest : Date) {
         this.value = value; this.earliest = earliest;
@@ -9,6 +15,7 @@ export class Window {
     earliest : Date;
 }
 
+// Create a window from an hours value.
 export function windowFromValue(value : number) : Window {
     let then = new Date(new Date().getTime() - value * 3600 * 1000);
     return new Window(value, then);
@@ -19,16 +26,19 @@ export function windowFromValue(value : number) : Window {
 })
 export class WindowService {
 
+    // Subject used to push Window updates
     subject : Subject<Window>;
 
     constructor(
     ) {
         this.subject = new Subject<Window>();
-	this.lastValue = undefined;
     }
 
+    // We keep the last value pushed out so that new subscribers can be
+    // given it.
     lastValue : Window;
 
+    // Push a new window update
     update(value : Window) : void {
 	if (this.lastValue == undefined || value != this.lastValue) {
             this.lastValue = value;
@@ -36,12 +46,12 @@ export class WindowService {
 	}
     }
 
+    // Subscribe to window updates.
     subscribe(f : any) {
         this.subject.subscribe(f);
 
         // Give subscribers last value.
 	if (this.lastValue != undefined) {
-
 	    f(this.lastValue);
 	}
     }
