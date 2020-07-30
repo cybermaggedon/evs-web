c2252b59907b24a649304fcaf34eaa5879f08e11
Cyber MacGeddon <cybermaggedon@gmail.com>
2020-07-30 19:01:02 +0100

    Made all currency Euros.  Mostly derived from locale settings, but hard-coded in charts.ts, needs fixing.

--- src/app/charts.ts
+++ src/app/charts.ts
@@ -2,19 +2,19 @@
 export function currencyTick(value, index, values) {
 
     if (value > 1000000000000){
-	return '$' + (value / 1000000000000).toFixed(1) + 'T';
+	return '€' + (value / 1000000000000).toFixed(1) + 'T';
     }
     if (value > 1000000000){
-	return '$' + (value / 1000000000).toFixed(1) + 'B';
+	return '€' + (value / 1000000000).toFixed(1) + 'B';
     }
     if (value > 1000000){
-	return '$' + (value / 1000000).toFixed(1) + 'M';
+	return '€' + (value / 1000000).toFixed(1) + 'M';
     }
     if (value > 1000){
-	return '$' + (value / 1000).toFixed(1) + 'k';
+	return '€' + (value / 1000).toFixed(1) + 'k';
     }
     
-    return '$' + value;
+    return '€' + value;
 }
 
 export function toxy(ds) {
