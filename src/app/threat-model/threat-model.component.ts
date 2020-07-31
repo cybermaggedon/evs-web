ddeb293186c927f2b241cc7bd0081780ce7ee6fe
Cyber MacGeddon <cybermaggedon@gmail.com>
2020-07-31 12:07:00 +0100

    Added an annotation to each threat element.

--- src/app/threat-model/threat-model.component.ts
+++ src/app/threat-model/threat-model.component.ts
@@ -16,52 +16,49 @@ export class ThreatModelComponent implements OnInit {
     threats = [
 	{
 	    id: "gsuite-threat-email",
+	    description: "email threats",
 	    risks: [
-		{
-		    id: "malware",
-		    score: 0.3
-		},
-		{
-		    id: "credential-theft",
-		    score: 0.2
-		}
+		{ id: "tor-exit", score: 0.5 },
+		{ id: "malware", score: 0.2 },
+		{ id: "credential-theft", score: 0.2 },
 	    ]
 	},
 	{
 	    id: "gsuite-threat-auth",
-	    risks: [
-		{
-		    id: "tor-exit",
-		    score: 0.8
-		}
-	    ]
+	    description: "auth threats",
+	    risks: [ {
+		    id: "tor-exit", score: 0.8
+	    } ]
 	},
 	{
 	    id: "research-threat",
+	    description: "IP threats",
 	    risks: [
-		{
-		    id: "tor-exit",
-		    score: 0.2
-		}
+		{ id: "tor-exit", score: 0.2 }
 	    ]
 	},
 	{
 	    id: "office-threat",
+	    description: "network threats",
 	    risks: [
 	    ]
 	},
 	{
 	    id: "payment-threat",
+	    description: "fraud threats",
 	    risks: [
+		{ id: "credential-theft", score: 0.1 },
 	    ]
 	},
 	{
 	    id: "prod-threat",
+	    description: "integrity threats",
 	    risks: [
 	    ]
 	},
 	{
 	    id: "it-threat",
+	    description: "device threats",
 	    risks: [
 	    ]
 	}
